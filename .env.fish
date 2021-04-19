d7528e39a2c5acb010b47ed454244ed132c93069
ArnaudAbreu <arnaud.abreu.p@gmail.com>
2021-04-20 00:12:10 +0200

    put personnal env files in gitignore

---
+++ .env.fish
@@ -0,0 +1,5 @@
+SLIDES=/Users/administrateur/Pictures/test_patchify_pathaia/Fish_test
+ANNOTS=/Users/administrateur/Pictures/test_patchify_pathaia/Fish_test
+REACT_APP_PORT=3000
+REACT_APP_FLASK_SERVER_IP=0.0.0.0
+REACT_APP_FLASK_SERVER_PORT=5000
